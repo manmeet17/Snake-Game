a570b6a95f74b36f605af49b6d61484734486c17
Manmeet Tarun <meetutarun16@gmail.com>
2017-08-14 19:53:01 +0530

    Game Variables

--- snake.py
+++ snake.py
@@ -6,8 +6,27 @@ import time
 
 check_error = pygame.init()
 if(check_error[1] != 0):
-	print ("Had {0} inititalizing error".format(check_error[1]))
+	print ("Faced {0} inititalizing error".format(check_error[1]))
 	sys.exit(-1)
 playSurface = pygame.display.set_mode((720,460))
 pygame.display.set_caption("Snake Game")
-time.sleep(5)
+
+red=pygame.Color(255,0,0)
+green=pygame.Color(0,255,0)
+black=pygame.Color(0,0,0)
+white=pygame.Color(255,255,255)
+brown=pygame.Color(165,42,42)
+
+# FPS controller
+fpsController=pygame.time.Clock()
+
+# Variables
+snakePos=[100,50]
+snakeBody=[[100,50],[90,50],[80,50]]
+foodPos=[random.randrange(1,72)*10,random.randrange(1,46)*10]
+foodSpawn=True
+
+direct='RIGHT'
+changeTo=direct
+
+
